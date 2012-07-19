3dec36f03797fa49352f4c48730a333a23988f3c
Ramon Smits <ramon@ramonsmits.com>
2012-07-19 15:53:11 +0200

    Updated incorrect company.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,9 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("master")]
-[assembly: AssemblyCompany("Company webcast")]
+[assembly: AssemblyCompany("http://ramonsmits.com")]
 [assembly: AssemblyProduct("Base64Encoder")]
 [assembly: AssemblyCopyright("Copyright © Ramon Smits 2012")]
 [assembly: AssemblyTrademark("")]
